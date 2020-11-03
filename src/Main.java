fb77f3733f192f14adc9de9ce5770ddf8ec54b1f
josep <jcuestav22@elpuig.xeill.net>
2020-11-03 17:29:39 +0100

    hola

--- src/Main.java
+++ src/Main.java
@@ -6,29 +6,27 @@ public class Main {
         while (J !=0){
             J++;
 
-        System.out.println("|--------------------|");
-        System.out.println("|    1.Products      |");
-        System.out.println("|    2.Orders        |");
-        System.out.println("|    3.Exit          |");
-        System.out.println("|--------------------|");
+        System.out.println("|----------------------|");
+        System.out.println("|    1.Products        |");
+        System.out.println("|    2.Orders          |");
+        System.out.println("|    3.Exit            |");
+        System.out.println("|----------------------|");
 
         Scanner scanner = new Scanner(System.in);
         int respuesta = scanner.nextInt();
+
         if (respuesta==1){
-        Menu menu = new Menu();
-        menu.menuproducto();
+            Menu menu = new Menu();
+            menu.menuproducto();
         }
-
         else if (respuesta==2){
-            //menu
-            // add order
-            // see order
-            //
+            Menu menu = new Menu();
+            menu.menupedidos();
         }
-
         else if (respuesta==3){
-            break;
 
+            System.out.println("DEW");
+            break;
         }
     }
 }
--- src/Menu.java
+++ src/Menu.java
@@ -5,30 +5,57 @@ public class Menu {
         int J2=1;
         while(1 !=0){
             J2++;
-        System.out.println("|----------------------|");
-        System.out.println("|    1.add new product |");
-        System.out.println("|    2.delete product  |");
-        System.out.println("|    3.update product  |");
-        System.out.println("|    4.list of products|");
-        System.out.println("|    5.back            |");
-        System.out.println("|----------------------|");
-        Scanner scanner = new Scanner(System.in);
-        int respuesta = scanner.nextInt();
-        if (respuesta==1){
+            System.out.println("|----------------------|");
+            System.out.println("|    1.Add new product |");
+            System.out.println("|    2.Delete product  |");
+            System.out.println("|    3.Update product  |");
+            System.out.println("|    4.List of products|");
+            System.out.println("|    5.Back            |");
+            System.out.println("|----------------------|");
+            Scanner scanner = new Scanner(System.in);
+            int respuesta = scanner.nextInt();
+            if (respuesta==1){
 
-        }
-        else if (respuesta==2){
+            }
+            else if (respuesta==2){
 
-        }
-        else if (respuesta==3){
+            }
+            else if (respuesta==3){
 
-        }
-        else if (respuesta==4){
+            }
+            else if (respuesta==4){
 
-        }
-        else if (respuesta==5){
-            break;
-        }
+            }
+            else if (respuesta==5){
+                break;
+            }
     }
+
 }
+    public void menupedidos(){
+        //menu
+        //
+        //
+        int J3=1;
+        while(1 !=0){
+            J3++;
+            System.out.println("|----------------------|");
+            System.out.println("|    1.Add order       |");
+            System.out.println("|    2.See order       |");
+            System.out.println("|    3.Back            |");
+            System.out.println("|----------------------|");
+            Scanner scanner = new Scanner(System.in);
+            int respuesta = scanner.nextInt();
+
+            if (respuesta==1){
+
+            }
+            else if (respuesta==2){
+
+            }
+            else if (respuesta==3){
+                break;
+            }
+    }
+    }
 }
